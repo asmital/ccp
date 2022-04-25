ad29888769cc228b74d018792db74f25fc3ed64a
Pranav Ballaney <ballaneypranav@gmail.com>
2022-04-25 15:02:39 +0530

    temp push

---
+++ parser.tab.hpp
@@ -0,0 +1,107 @@
+/* A Bison parser, made by GNU Bison 3.7.6.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_PARSER_TAB_HPP_INCLUDED
+# define YY_YY_PARSER_TAB_HPP_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    NUM = 258,                     /* NUM  */
+    BOOL = 259,                    /* BOOL  */
+    OPP = 260,                     /* OPP  */
+    CLP = 261,                     /* CLP  */
+    OPB = 262,                     /* OPB  */
+    CLB = 263,                     /* CLB  */
+    OPSB = 264,                    /* OPSB  */
+    CLSB = 265,                    /* CLSB  */
+    CMA = 266,                     /* CMA  */
+    ENDSEC = 267,                  /* ENDSEC  */
+    ADD = 268,                     /* ADD  */
+    SUB = 269,                     /* SUB  */
+    MUL = 270,                     /* MUL  */
+    DIV = 271,                     /* DIV  */
+    ASG = 272,                     /* ASG  */
+    EXP = 273,                     /* EXP  */
+    COL = 274,                     /* COL  */
+    GT = 275,                      /* GT  */
+    LT = 276,                      /* LT  */
+    NE = 277,                      /* NE  */
+    EQ = 278,                      /* EQ  */
+    GTE = 279,                     /* GTE  */
+    LTE = 280,                     /* LTE  */
+    UPDATE_TK = 281,               /* UPDATE_TK  */
+    SETUP_TK = 282,                /* SETUP_TK  */
+    LET = 283,                     /* LET  */
+    IF = 284,                      /* IF  */
+    THEN = 285,                    /* THEN  */
+    ELSE = 286,                    /* ELSE  */
+    ENDIF = 287,                   /* ENDIF  */
+    KEYBD = 288,                   /* KEYBD  */
+    ID = 289,                      /* ID  */
+    STR = 290,                     /* STR  */
+    EOS = 291                      /* EOS  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_PARSER_TAB_HPP_INCLUDED  */
